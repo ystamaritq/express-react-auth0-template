d71f3e4f3d9925bf87ce9a1e5e9bfcd1e1e582da
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 14:43:38 -0500

    moved env config

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
-
 const express = require("express");
 const routes = require("./controllers");
 const app = express();
 const morgan = require("morgan");
+require("dotenv").config();
 
 var db = require("./models");
 
